ab5eaf1a432e268834b11c8c7a8f5b194f4815a7
Hoya324 <hoyana1225@gmail.com>
2025-11-28 01:53:03 +0900

    Initial Commit

---
+++ Aumeno/AumenoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AumenoApp.swift
+//  Aumeno
+//
+//  Created by 나경호 on 11/28/25.
+//
+
+import SwiftUI
+
+@main
+struct AumenoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
